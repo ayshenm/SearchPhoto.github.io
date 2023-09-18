75cbb15f2d1da47004b83674bc3e3066fc7f61da
Aisha Mirza <84517249+ayshenm@users.noreply.github.com>
2023-09-18 22:21:14 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+const formWrapper = document.querySelector(".form-wrapper");
+const form = document.querySelector("#form");
+const searchInput = document.querySelector("#searchInput");
+const buttomWrapper = document.querySelector(".button-wrapper");
+const searchButton = document.querySelector("#searchButton");
+const clearButton = document.querySelector("#clearButton");
+const imageListWrapper = document.querySelector(".imagelist-wrapper");
+
+runEventListeners();
+
+function runEventListeners() {
+  form.addEventListener("submit", search);
+  clearButton.addEventListener("click", clear);
+}
+
+function clear() {
+  searchInput.value = "";
+  // Array.from(imageListWrapper.children).forEach((child)=>{
+  //     child.remove();
+  // })
+  imageListWrapper.innerHTML = "";
+}
+function search(e) {
+  const value = searchInput.value.trim(); //inputa yazdigimi axtara basanda cixardir
+  fetch(`https://api.unsplash.com/search/photos?query=${value}`, {
+    method: "GET",
+    headers: {
+      Authorization: "Client-ID zA2q2z3CHBL4pZW-CUlPYPtVvNXbZREfhCMEwTzsOow",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      Array.from(data.results).forEach((image) => {
+        addImageToUI(image.urls.small);
+      });
+    })
+    .catch((err) => console.log(err));
+
+  e.preventDefault();
+}
+
+function addImageToUI(url) {
+  const div = document.createElement("div");
+  div.className = "card";
+  
+
+  const img = document.createElement("img");
+  img.setAttribute("src", url);
+  img.height = "400";
+  img.width = "400";
+
+  div.appendChild(img);
+  imageListWrapper.appendChild(div);
+}
